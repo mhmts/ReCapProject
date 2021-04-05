96a4e301aff458dfb8b1c8e3f2e3a44841888c12
mhmts <78983466+mhmts@users.noreply.github.com>
2021-04-05 22:37:09 +0300

    12.Gün Ödev2

---
+++ Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CustomerValidator:AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(b => b.CompanyName).NotEmpty();
+            RuleFor(b => b.CompanyName).MinimumLength(3);
+            RuleFor(b => b.CompanyName).Must(StartWithA).WithMessage("İsimler A harfi ile başlamalı");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+
+}
